e94f4a98b022159d8158819111d5d26334aa3a77
Aleksandra Sindiaeva <morning-sun@Aleksandras-MacBook-Air.local>
2018-02-12 17:29:49 +0700

    localization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,12 @@ version = u'1.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
+
 language = 'ru'
+
+locale_dirs = ['locales/']
+
+gettext_compact = False
 
 
 # List of patterns, relative to source directory, that match files and
