1ca2dead83ef07c4bbf4d1931e5f3d809cf0b383
sasha-carlos <a.sindyaeva@ntechlab.com>
2018-10-05 16:28:14 +0700

    tsv_builder

--- docs/source/_scripts/tsv_builder.py
+++ docs/source/_scripts/tsv_builder.py
@@ -11,9 +11,11 @@ def create_file(path):
         for file in os.listdir(path):
             if not file.startswith('.') and file.endswith(('.jpg', '.png', '.jpeg')) \
                     and os.path.isfile(os.path.abspath(path) + '/' + file):
-                            print('%s/%s \t %s' % (os.path.abspath(path), file, os.path.splitext(file)[0]))
-                            f.writelines('%s/%s \t %s\n' % (os.path.abspath(path), file, os.path.splitext(file)[0]))
+                            print('%s/%s\t %s' % (os.path.abspath(path), file, os.path.splitext(file)[0]))
+                            f.writelines('%s/%s\t %s\n' % (os.path.abspath(path), file, os.path.splitext(file)[0]))
 
 
 path = sys.argv[1]
 create_file(path)
+
+
