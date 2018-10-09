83a74a6587fb54f22d05effc6b13503571fa0c09
sasha-carlos <a.sindyaeva@ntechlab.com>
2018-10-09 17:45:09 +0700

    mobile

--- docs/source/_scripts/tsv_builder.py
+++ docs/source/_scripts/tsv_builder.py
@@ -1,21 +1,13 @@
 import os
 import sys
 
-
-print('sys.argv = ', sys.argv[1])
-
-
-def create_file(path):
-    open("images.tsv", 'w').close()
-    with open("images.tsv", "a") as f:
-        for file in os.listdir(path):
-            if not file.startswith('.') and file.endswith(('.jpg', '.png', '.jpeg')) \
-                    and os.path.isfile(os.path.abspath(path) + '/' + file):
-                            print('%s/%s\t %s' % (os.path.abspath(path), file, os.path.splitext(file)[0]))
-                            f.writelines('%s/%s\t %s\n' % (os.path.abspath(path), file, os.path.splitext(file)[0]))
-
-
-path = sys.argv[1]
-create_file(path)
+if __name__=="__main__":
+    with open('images.tsv', 'w') as out:
+        for arg in sys.argv[1:]:
+            for root, _, files in os.walk(arg):
+                for fname in files:
+                    basename, ext = os.path.splitext(fname)
+                    if ext.lower() in ('.jpg', '.jpeg', '.png', '.webp'):
+                        print("%s\t%s" % (os.path.join(root, fname), basename), file=out)
 
 
